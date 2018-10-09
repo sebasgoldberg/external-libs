4be997bf5bbddd4fb008c165bdcae7034bc93cde
Juan Sebastian Goldberg <sebas.goldberg@gmail.com>
2018-10-09 14:33:24 -0300

    Namespace modification.

--- webapp/library.js
+++ webapp/library.js
@@ -2,7 +2,7 @@
 import polyfill from "./lib/polyfill";
 
 let oLibrary = sap.ui.getCore().initLibrary({
-    name: "cencosud.libs.external.polyfill",
+    name: "iamsoft.libs.external.polyfill",
     noLibraryCSS: true,
 });
 
